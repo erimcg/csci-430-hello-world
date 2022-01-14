586f98d46e232ca8110ac4db5f98359ce5402fdf
Eric McGregor <eric@n0code.net>
2022-01-14 09:48:04 -0500

    adding static about page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require('express') 
-const app = express() 
+const app = express()
 
 const port = process.env.PORT || 3000 
 
@@ -7,16 +7,15 @@ const path = require('path')
 const dir = path.join(__dirname, "../public")
 app.use(express.static(dir))
 
-app.get('', (req, res) => { 
-    res.send('Hello Eric') 
-}) 
 
-app.get('/about', (req, res) => {
-    res.sendFile(path.join(dir, "about.html"))
-    //res.send('Hello Test') 
-  })
+app.get('', (req, res) => { 
+    let o = {x: 1, y: 2}
+
+    res.send(o)
+})
 
 app.listen(port, () => { 
     console.log('Server is up on port ' + port) 
 })
 
+console.log(__dirname)
