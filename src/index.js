1099def4f2ec61cc0e34ce2e2fd006120a56ec77
Eric McGregor <eric@n0code.net>
2022-01-14 20:14:49 -0500

    remove static server

--- src/index.js
+++ src/index.js
@@ -3,15 +3,8 @@ const app = express()
 
 const port = process.env.PORT || 3000 
 
-const path = require('path')
-const dir = path.join(__dirname, "../public")
-app.use(express.static(dir))
-
-
-app.get('', (req, res) => { 
-    let o = {x: 1, y: 2}
-
-    res.send(o)
+app.get('', (req, res) => {
+    res.send('Hello world!')
 })
 
 app.listen(port, () => { 
